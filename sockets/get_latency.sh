af8d31374ef5fdd6917747d345cac4a9daa053b8
Albert Dang <albert.dang46@gmail.com>
2021-05-28 19:39:59 -0700

    get_latency accepts ip argument

--- sockets/get_latency.sh
+++ sockets/get_latency.sh
@@ -4,12 +4,12 @@ for i in $(seq 1000 1000 1000000)
 do
     while pgrep -x server; do sleep 5; done;
     ./server char $i &
-    ./client char $i >> latency_data.txt
+    ./client char $i $1 >> latency_data.txt
 done
 
 for i in $(seq 1000 1000 1000000)
 do
     while pgrep -x server; do sleep 5; done;
     ./server node $i &
-    ./client node $i >> latency_data_tree.txt
+    ./client node $i $1 >> latency_data_tree.txt
 done 
